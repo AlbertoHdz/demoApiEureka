d0c1781d3fd5c895b61453b9845bf8bb922eaa8d
Albert Hdz <alberto_8565@hotmail.com>
2024-08-16 22:31:03 -0600

    configuracion
    se suben los archivos de configuración

---
+++ src/test/java/com/docker/netflix/dockerspringnetflix_ZuulService/DockerspringnetflixZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.netflix.dockerspringnetflix_ZuulService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerspringnetflixZuulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
